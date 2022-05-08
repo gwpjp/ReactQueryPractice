d762a38d9a1cbd7a0f5f553001b64c45b152c434
John Pearson <john.pearson.02@gmail.com>
2022-05-08 10:24:33 -0400

    Greeter function working on blockchain.

--- random-number/hardhat/hardhat.config.js
+++ random-number/hardhat/hardhat.config.js
@@ -1,8 +1,8 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -17,5 +17,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
+  paths: {
+    artifacts: '../src/artifacts', // this is where our compiled contracts will go
+  },
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+  },
 };
--- random-number/src/App.js
+++ random-number/src/App.js
@@ -1,5 +1,9 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 const fetchNumber = () =>
   fetch(
@@ -68,8 +72,55 @@ const UseEffectRandom = () => {
   );
 };
 export default function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  // request access to the user's account. This works regardless of the wallet you're using.
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        const data = await contract.greet();
+        console.log('data: ', data);
+      } catch (err) {
+        console.log('Error: ', err);
+      }
+    }
+  }
+
+  // call the smart contract, send an update
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
     <div>
+      <header className="App-header">
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input
+          onChange={(e) => setGreetingValue(e.target.value)}
+          placeholder="Set greeting"
+        />
+      </header>
       <h2>useEffect</h2>
       <UseEffectRandom />
       <h2>React Query</h2>
