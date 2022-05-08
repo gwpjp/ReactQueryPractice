552fe7fde19934c5826f12ec5b3a289197b29b9c
John Pearson <john.pearson.02@gmail.com>
2022-05-07 20:35:30 -0400

    Fetch working w/ React Query.

--- github-fetch/src/App.tsx
+++ github-fetch/src/App.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import './App.css';
 
+const fetchUser = (username: string) => fetch(`https://api.github.com/users/${username}`).then((res) => res.json());
+
+type GithubUserProps = {
+  username: string;
+};
+
+function GithubUser({ username }: GithubUserProps) {
+  const userQuery = useQuery([username], () => fetchUser(username));
+
+  const { data } = userQuery;
+
+  if (userQuery.isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (userQuery.isError) {
+    let err = '';
+    if (userQuery.error instanceof Error) {
+      err = userQuery.error.message;
+    }
+    return (
+      <p>
+        Error:
+        {err}
+      </p>
+    );
+  }
+
+  return <p>{JSON.stringify(data, null, 2)}</p>;
+}
+
 function App() {
-  return <div />;
+  return (
+    <div>
+      <GithubUser username="gwpjp" />
+    </div>
+  );
 }
 
 export default App;
