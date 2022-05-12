02cd119a0d6a4350595f68b1479faf606880aaee
John Pearson <john.pearson.02@gmail.com>
2022-05-12 13:37:25 -0400

    Added styling

--- random-number/src/App.js
+++ random-number/src/App.js
@@ -188,7 +188,7 @@ export default function App() {
 
   return (
     <div style={{ width: '100%', display: 'flex', alignContent: 'stretch' }}>
-      <div style={{ padding: 20, margin: 20 }}>
+      <div style={{ padding: 10, margin: 0 }}>
         <h2>Greeting from Blockchain</h2>
         <h4>Fetch:</h4>
         <ReactQueryFetchGreeting />
@@ -208,11 +208,11 @@ export default function App() {
       <h2>useEffect</h2>
       <UseEffectRandom />
        */}
-      <div style={{ padding: 20, margin: 20 }}>
+      <div style={{ padding: 10, marginLeft: 20 }}>
         <h2>Random Number from API</h2>
         <ReactQueryRandom />
       </div>
-      <div style={{ padding: 20, margin: 20 }}>
+      <div style={{ padding: 10, marginLeft: 20 }}>
         <h2>User from Metamask</h2>
         <ReactQueryUser />
       </div>
